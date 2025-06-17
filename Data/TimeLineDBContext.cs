a2a1e62b1d687651ec4252881917b8e660a75764
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-17 17:02:07 +0600

    TimeLineDB context -- added

---
+++ Data/TimeLineDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeLineViwer.Models;
+
+namespace TimeLineViwer.Data
+{
+    public class TimeLineDBContext : DbContext
+    {
+        public TimeLineDBContext(DbContextOptions<TimeLineDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }  
+    }
+}
