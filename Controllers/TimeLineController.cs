c95e1a44b3d0c7c613bfc1cecbf34aec047b5b89
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-17 16:22:16 +0600

    IHubContext injected in TimeLineController

---
+++ Controllers/TimeLineController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using TimeLineViwer.Hub;
+
+namespace TimeLineViwer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeLineController : ControllerBase
+    {
+        private IHubContext<MessageHub, IMessageHubClient> messageHub;
+        public TimeLineController(IHubContext<MessageHub, IMessageHubClient> _messageHub)
+        {
+            messageHub = _messageHub;
+
+        }
+        [HttpPost]
+        [Route("posts")]
+        public string Get()
+        {
+            List<string> posts = new List<string>();
+            
+            messageHub.Clients.All.SendOffersToUser(posts);
+            return "posts created!";
+        }
+    }
+}
