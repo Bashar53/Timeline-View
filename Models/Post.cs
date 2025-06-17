96dcd8fb6ee8dd412cf216827d78ef63f0ce4773
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-17 16:55:17 +0600

    Post Model -- added

---
+++ Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeLineViwer.Models;
+
+public class Post
+{
+    [Key]
+    public int PostId { get; set; }
+    public int UserId { get; set; }
+    [Required(ErrorMessage = "Content is required.")]
+    [StringLength(1000, ErrorMessage = "Content cannot exceed 1000 characters.")]
+    public string Content { get; set; }
+    [StringLength(500, ErrorMessage = "MediaURL cannot exceed 500 characters.")]
+    public string MediaURL { get; set; }    
+    public string TimeStamp { get; set; }
+    public ICollection<User>? Users { get; set; }   
+
+
+}
