a67a419ffabf57a7322df9c693e6e9fe3e9a3e2b
Md Bashar <bashar.iiuc77@gmail.com>
2025-06-17 16:55:27 +0600

    user Model -- added

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeLineViwer.Models
+{
+    public class User
+    {
+        [Key]   
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+       
+    }
+}
